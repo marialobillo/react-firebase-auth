8d7fcf721fdd06d43594a33277319e6edcfaf24e
marialobillo <maria.lobillo.santos@gmail.com>
2018-08-15 10:10:27 +0200

    firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,31 @@
+import firebase form 'firebase';
+import 'firebase/auth';
+
+const prodConfig = {
+   apiKey: "AIzaSyApycS0QXSwMSErZgcgTem3X0NWIRhxMiU",
+   authDomain: "react-notes-aae60.firebaseapp.com",
+   databaseURL: "https://react-notes-aae60.firebaseio.com",
+   projectId: "react-notes-aae60",
+   storageBucket: "react-notes-aae60.appspot.com",
+   messagingSenderId: "56448092445"
+ };
+ const devConfig = {
+    apiKey: "AIzaSyApycS0QXSwMSErZgcgTem3X0NWIRhxMiU",
+    authDomain: "react-notes-aae60.firebaseapp.com",
+    databaseURL: "https://react-notes-aae60.firebaseio.com",
+    projectId: "react-notes-aae60",
+    storageBucket: "react-notes-aae60.appspot.com",
+    messagingSenderId: "56448092445"
+  };
+
+const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+
+ if(!firebase.apps.length){
+   firebase.initializeApp(config);
+ }
+
+const auth = firebase.auth();
+
+export{
+  auth,
+};
