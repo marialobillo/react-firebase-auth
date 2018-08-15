aea79c7ec459dd9afa20e25c2b8fee5cdb7e0054
marialobillo <maria.lobillo.santos@gmail.com>
2018-08-15 10:21:02 +0200

    firebase setup

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -7,3 +7,15 @@ export const doCreateUserWithEmailAndPassword = (email, password) =>
 // Sign In
 export const doSignInWithEmailAndPassword = (email, password) =>
   auth.doSignInWithEmailAndPassword(email, password);
+
+// Sign out
+export const doSignOut = () =>
+  auth.signOut();
+
+// Password Reset
+export const doPasswordReset = (email) =>
+  auth.sendPasswordResetEmail(email);
+
+// Password Change
+export const doPasswordUpdate = (password) =>
+  auth.currentUser.updatePassword(password);
